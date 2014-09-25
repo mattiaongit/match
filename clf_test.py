7ad75e447c09d39e1de5674483b518e98eddab6e
mattiaongit <mattiadmr@gmail.com>
2014-09-25 18:29:50 +0200

    Update clf_test.py

--- clf_test.py
+++ clf_test.py
@@ -103,3 +103,34 @@ recall = sklearn.metrics.recall_score(y_test,y_pred)
 f1 = 2 * ( (precision * recall) / (precision + recall) )
 print("{0} prec: {1}, rec: {2}, f1: {3}".format(key,precision,recall,f1))
 
+
+#GRID SEARCH
+
+# tuned_parameters = [{'kernel': ['rbf'], 'gamma': [1e-3, 1e-4],
+#                      'C': [1, 10, 100, 1000]},
+#                     {'kernel': ['linear'], 'C': [1, 10, 100, 1000]}]
+
+# scores = ['precision', 'recall']
+
+# for score in scores:
+#   print("# Tuning hyper-parameters for %s" % score)
+#   print()
+#   clf = GridSearchCV(SVC(C=1), tuned_parameters, cv=5, scoring=score)
+#   clf.fit(X_train, y_train)
+#   print("Best parameters set found on development set:")
+#   print()
+#   print(clf.best_estimator_)
+#   print()
+#   print("Grid scores on development set:")
+#   print()
+#   for params, mean_score, scores in clf.grid_scores_:
+#     print("%0.3f (+/-%0.03f) for %r" % (mean_score, scores.std() / 2, params))
+#   print()
+#   print("Detailed classification report:")
+#   print()
+#   print("The model is trained on the full development set.")
+#   print("The scores are computed on the full evaluation set.")
+#   print()
+#   y_true, y_pred = y_test, clf.predict(X_test)
+#   print(classification_report(y_true, y_pred))
+#   print()
